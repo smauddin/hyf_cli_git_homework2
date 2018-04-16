f50059dbeaf97bdbef2850278ed8c8b41d588687
Anowar Uddin <lenin3636@gmil.com>
2018-04-16 09:11:00 +0200

    script created

---
+++ anowar_uddin.sh
@@ -0,0 +1,19 @@
+mkdir cabbage
+cd cabbage
+pwd
+touch orange.txt
+echo "Hello" > orange.txt
+echo "Hello" >> orange.txt
+echo "Hello" >> orange.txt
+echo "Hello" >> orange.txt
+echo "Hello" >> orange.txt
+cat orange.txt | head -1 > 1.txt
+cat orange.txt | head -2 > 2.txt
+cat orange.txt | head -3 > 3.txt
+cat orange.txt | head -4 > 4.txt
+cp orange.txt 5.txt
+echo "cat" > pets.txt
+echo "dog" >> pets.txt
+echo "hamster" >> pets.txt
+cat pets.txt
+ls
